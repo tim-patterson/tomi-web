a77784f21bb0c57c4c5cdb45551d73fbcce621af
Tim Patterson <tim.wanaka@gmail.com>
2020-11-07 18:33:43 +1300

    Move sprite around the screen

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,4 +1,5 @@
 use web_sys::{ CanvasRenderingContext2d, HtmlImageElement};
+use wasm_bindgen::__rt::std::rc::Rc;
 
 /// A wrapper around an html canvas.
 pub struct Canvas {
@@ -30,7 +31,86 @@ impl Canvas {
         self.inner.fill_text(text, self.width as f64/2.0, self.height as f64/2.0).unwrap();
     }
 
-    pub fn draw_sprite(&mut self, sprite: &HtmlImageElement, x: u32, y: u32) {
-        self.inner.draw_image_with_html_image_element(sprite, x as f64, y as f64).unwrap();
+    pub fn draw_sprite_with_alignment(&mut self, sprite_sheet: &SpriteSheet, sprite_idx: u32, x: u32, y: u32, alignment: Alignment) {
+        let grid_x = sprite_idx % sprite_sheet.rc.x_count;
+        let grid_y = sprite_idx / sprite_sheet.rc.x_count;
+
+        let sx = grid_x * sprite_sheet.rc.x_size;
+        let sy = grid_y * sprite_sheet.rc.y_size;
+
+        let (left, top) = match alignment {
+            Alignment::TopLeft => (x, y),
+            Alignment::Centered => (x - sprite_sheet.rc.x_size / 2, y - sprite_sheet.rc.y_size / 2)
+        };
+
+        self.inner.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+            &sprite_sheet.rc.inner,
+            sx as f64,
+            sy as f64,
+            sprite_sheet.rc.x_size as f64,
+            sprite_sheet.rc.y_size as f64,
+            left as f64,
+            top as f64,
+            sprite_sheet.rc.x_size as f64,
+            sprite_sheet.rc.y_size as f64,
+        ).unwrap();
+    }
+}
+
+/// Where we consider the x/y 0,0 to be when rendering to the
+/// canvas
+pub enum Alignment {
+    TopLeft,
+    Centered
+}
+
+
+
+
+
+/// A wrapper around an image, an image may contain multiple sprites
+#[derive(Clone)]
+pub struct SpriteSheet {
+    rc: Rc<SpriteSheetInner>
+}
+
+struct SpriteSheetInner {
+    inner: HtmlImageElement,
+    x_count: u32,
+    y_count: u32,
+    /// Size in pixels (source) of a single sprite
+    x_size: u32,
+    /// Size in pixels (source) of a single sprite
+    y_size: u32,
+}
+
+
+impl SpriteSheet {
+    pub fn new_single(inner: HtmlImageElement) -> Self {
+        let x_size = inner.natural_width();
+        let y_size = inner.natural_height();
+        SpriteSheet {
+            rc: Rc::new(SpriteSheetInner {
+                inner,
+                x_count: 1,
+                y_count: 1,
+                x_size,
+                y_size
+            })
+        }
+    }
+
+    pub fn new_grid(inner: HtmlImageElement, x_count: u32, y_count: u32) -> Self {
+        let x_total_size = inner.natural_width();
+        let y_total_size = inner.natural_height();
+        SpriteSheet {
+            rc: Rc::new(SpriteSheetInner {
+                inner,
+                x_count,
+                y_count,
+                x_size: x_total_size / x_count,
+                y_size: y_total_size / y_count
+            })
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,46 @@ mod canvas;
 
 use wasm_bindgen::prelude::*;
 use web_sys::{HtmlImageElement, HtmlAudioElement, CanvasRenderingContext2d};
-use crate::canvas::Canvas;
+use crate::canvas::{Canvas, SpriteSheet, Alignment};
 
+/// The top level game
 #[wasm_bindgen]
 pub struct Game {
     canvas: Canvas,
     assets: Assets,
-    state: State
+    status: Status,
+    tomi: GameObject
 }
 
-enum State {
+/// Enum for if the game has started or not yet
+enum Status {
     Waiting,
     Running
 }
 
-pub struct Assets {
-    sprt_tomi: HtmlImageElement,
+/// All the assets used for the game
+struct Assets {
+    sprt_tomi: SpriteSheet,
     snd_step: HtmlAudioElement,
     snd_background: HtmlAudioElement
+}
+
+/// An object in the game
+struct GameObject {
+    x: u32,
+    y: u32,
+    direction: Direction,
+    speed: u32,
+    sprite: SpriteSheet
+}
+
+
+enum Direction {
+    Stationary,
+    Left,
+    Right,
+    Up,
+    Down,
 }
 
 #[wasm_bindgen]
@@ -28,61 +50,84 @@ impl Game {
     pub async fn new(canvas_context: CanvasRenderingContext2d) -> Result<Game, JsValue> {
         let mut canvas = Canvas::new(canvas_context);
         canvas.text("Loading");
-
-        let mut game = Game {
-            canvas,
-            assets: Game::load_assests().await?,
-            state: State::Waiting
-        };
+        let assets = Game::load_assests().await?;
 
         // Due to annoying websites browsers made it a rule that you had to
         // interact with a website before we can start to play sounds etc.
-        game.canvas.clear();
-        game.canvas.text("Press any key to start");
+        canvas.clear();
+        canvas.text("Press any key to start");
+        canvas.draw_sprite_with_alignment(&assets.sprt_tomi, 0, 300, 180, Alignment::Centered);
 
-        Ok(game)
+        let tomi = GameObject { x: 300, y: 300, direction: Direction::Stationary, speed: 5, sprite: assets.sprt_tomi.clone()};
+
+        Ok(Game {
+            canvas,
+            assets,
+            status: Status::Waiting,
+            tomi
+        })
     }
 
     async fn load_assests() -> Result<Assets, JsValue> {
-        let sprt_tomi = load_image("sprites/tomi_stationary.png");
+        let sprt_tomi = load_image("sprites/tomi_walking.png");
         let snd_step = load_sound("sounds/step.mp3");
         let snd_background = load_sound("sounds/background.mp4");
         Ok(Assets {
-            sprt_tomi: sprt_tomi.await?,
+            sprt_tomi: SpriteSheet::new_grid(sprt_tomi.await?,4, 1),
             snd_step: snd_step.await?,
             snd_background: snd_background.await?
         })
     }
 
-    /// A single tick of the game
+    /// A single tick of the game(60Hz)
     #[wasm_bindgen]
     pub fn tick(&mut self, pressed_keys: &[i16]) {
-        match self.state {
-            State::Waiting => {
+        match self.status {
+            Status::Waiting => {
                 if !pressed_keys.is_empty() {
                     self.assets.snd_background.set_loop(true);
                     self.assets.snd_background.play().ok();
-                    self.state = State::Running;
+                    self.status = Status::Running;
                 }
             }
-            State::Running => {
-                if !pressed_keys.is_empty() {
-                    self.assets.snd_step.play().ok();
+            Status::Running => {
+                self.tomi.direction = if pressed_keys.contains(&37) {
+                    Direction::Left
+                } else if pressed_keys.contains(&39) {
+                    Direction::Right
+                } else if pressed_keys.contains(&38) {
+                    Direction::Up
+                } else if pressed_keys.contains(&40) {
+                    Direction::Down
+                } else {
+                    Direction::Stationary
+                };
+
+                // Update actual x/y's for tomi
+                match self.tomi.direction {
+                    Direction::Left => self.tomi.x -= self.tomi.speed,
+                    Direction::Right => self.tomi.x += self.tomi.speed,
+                    Direction::Up => self.tomi.y -= self.tomi.speed,
+                    Direction::Down => self.tomi.y += self.tomi.speed,
+                    Direction::Stationary => {}
                 }
             }
         }
     }
 
+    /// Render out to canvas(hopefully 60FPS!)
     #[wasm_bindgen]
     pub fn render(&mut self) {
-        if let State::Running = self.state {
+        if let Status::Running = self.status {
             self.canvas.clear();
+            self.canvas.draw_sprite_with_alignment(&self.tomi.sprite,
+                2, self.tomi.x, self.tomi.y, Alignment::TopLeft
+            )
         }
     }
 }
 
 // FFI's
-
 
 async fn load_sound(src: &str) -> Result<HtmlAudioElement, JsValue> {
     _load_sound(src).await.map(HtmlAudioElement::from)
